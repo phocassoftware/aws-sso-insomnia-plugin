43dc2d024d4904a1645589d97a26bf5629341f4d
Callum Galbreath <callum.galbreath@phocassoftware.com>
2021-08-30 16:49:12 +1200

    * Added updated details to README.md plus screenshots * Added prettier to the project as a dev dependency, and formatted accordingly

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# aws-sso-insomnia-plugin
-Retrieves credentials populated by aws sso login for use in requests
+# AWS SSO Insomnia Plugin
+
+A small plugin to retrieve AWS SSO credentials for signing IAMv4 authenticated requests.
+
+### Prerequisites
+
+- [AWS CLI V2](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html)
+- An AWS account with [SSO sign-on](https://docs.aws.amazon.com/singlesignon/latest/userguide/getting-started.html) enabled
+- An [SSO named profile](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html) configured using the AWS CLI
+
+### Usage
+
+If the prerequisites have been satisfied you should be able to perform an SSO login using:
+
+`aws sso login --profile <YOUR PROFILE>`
+
+To use the plugin within Insomnia, select the `AWS` authentication type for your request.
+
+The plugin is to be used from within the `ACCESS KEY ID`, `SECRET ACCESS KEY`, and `SESSION TOKEN` fields:
+
+Within a selected field, type `aws` and wait for autocomplete to provide variable suggestions. You should be able to select `AWS SSO` to fill in the field.
+
+![img.png](images/readme/field-autocomplete.png)
+
+Once you have populated a field, you can edit it by clicking on it. You need to select both a profile and an item for each tag.
+
+Make sure you select the item that corresponds to the field you are editing (e.g. if you are editing the `ACCESS KEY ID` field, make sure to select `Access Key Id` as the item.)
+
+You can hardcode the profile per field if you wish, but it is much more ergonomic to use an Insomnia environment variable. You can select an environment variable by clicking the gear icon next to the `Profile` dialog.
+
+Once configured you shouldn't need to adjust your individual fields. If you have used an environment variable for the profile, changing it will propagate through all of your requests configured to use this plugin.
+
+![img.png](images/readme/tag-editing.png)
--- src/plugin.ts
+++ src/plugin.ts
@@ -2,87 +2,91 @@ import { fromSSO } from "@aws-sdk/credential-provider-sso";
 import { CredentialProvider, Credentials } from "@aws-sdk/types";
 
 enum CredentialPropertyEnum {
-    accessKeyId,
-    secretAccessKey,
-    sessionToken
+  accessKeyId,
+  secretAccessKey,
+  sessionToken,
 }
 
 type CredentialProperty = keyof typeof CredentialPropertyEnum;
 type ProfileDetails = {
-    name : string,
-    provider : CredentialProvider;
-    credentials : Credentials;
-}
+  name: string;
+  provider: CredentialProvider;
+  credentials: Credentials;
+};
 
 let profiles = new Map<string, ProfileDetails>();
 
 async function getProfileDetails(profile: string) {
-    let details = profiles.get(profile);
-    if (details === undefined) {
-        // Only create the SSO Provider once per profile
-        let provider = fromSSO({profile: profile});
-        details = {
-            name : profile,
-            provider : provider,
-            credentials : await provider()
-        }
-        profiles.set(profile, details);
-    }
-    return details;
+  let details = profiles.get(profile);
+  if (details === undefined) {
+    // Only create the SSO Provider once per profile
+    let provider = fromSSO({ profile: profile });
+    details = {
+      name: profile,
+      provider: provider,
+      credentials: await provider(),
+    };
+    profiles.set(profile, details);
+  }
+  return details;
 }
 
 async function getFreshCredentials(details: ProfileDetails) {
-    if (details.credentials &&
-        details.credentials.expiration &&
-        details.credentials.expiration < new Date()) {
-        // If we have expired credentials, refresh
-        details = {
-            name : details.name,
-            provider : details.provider,
-            credentials : await details.provider()
-        };
-        profiles.set(details.name, details);
-    }
-    return details.credentials;
+  if (
+    details.credentials &&
+    details.credentials.expiration &&
+    details.credentials.expiration < new Date()
+  ) {
+    // If we have expired credentials, refresh
+    details = {
+      name: details.name,
+      provider: details.provider,
+      credentials: await details.provider(),
+    };
+    profiles.set(details.name, details);
+  }
+  return details.credentials;
 }
 
 export const templateTags = [
-    {
-        name: 'aws_sso',
-        displayName: 'AWS SSO',
-        liveDisplayName: (args: any) => {
-            return `${args[0].value} - ${args[1].value}`;
-        },
-        description: 'AWS SSO Credential Loader',
-        args: [
-            {
-                displayName: 'Profile',
-                type: 'string',
-                defaultValue: 'default',
-                modelType: 'model'
-            }, {
-                displayName: 'Item',
-                type: 'enum',
-                options: [
-                    {
-                        displayName: 'Access Key Id',
-                        value: CredentialPropertyEnum[CredentialPropertyEnum.accessKeyId],
-                    },
-                    {
-                        displayName: 'Secret Access Key',
-                        value: CredentialPropertyEnum[CredentialPropertyEnum.secretAccessKey],
-                    },
-                    {
-                        displayName: 'Session Token',
-                        value: CredentialPropertyEnum[CredentialPropertyEnum.sessionToken],
-                    },
-                ]
-            }
+  {
+    name: "aws_sso",
+    displayName: "AWS SSO",
+    liveDisplayName: (args: any) => {
+      return `${args[0].value} - ${args[1].value}`;
+    },
+    description: "Credential Loader",
+    args: [
+      {
+        displayName: "Profile",
+        type: "string",
+        defaultValue: "default",
+        modelType: "model",
+      },
+      {
+        displayName: "Item",
+        type: "enum",
+        options: [
+          {
+            displayName: "Access Key Id",
+            value: CredentialPropertyEnum[CredentialPropertyEnum.accessKeyId],
+          },
+          {
+            displayName: "Secret Access Key",
+            value:
+              CredentialPropertyEnum[CredentialPropertyEnum.secretAccessKey],
+          },
+          {
+            displayName: "Session Token",
+            value: CredentialPropertyEnum[CredentialPropertyEnum.sessionToken],
+          },
         ],
-        async run(_context: object, profile: string, property: CredentialProperty) {
-            let details = await getProfileDetails(profile);
-            let credentials = await getFreshCredentials(details);
-            return credentials[property];
-        },
-    }
+      },
+    ],
+    async run(_context: object, profile: string, property: CredentialProperty) {
+      let details = await getProfileDetails(profile);
+      let credentials = await getFreshCredentials(details);
+      return credentials[property];
+    },
+  },
 ];
