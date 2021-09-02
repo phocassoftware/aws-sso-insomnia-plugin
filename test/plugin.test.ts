e973ea04ae46f94ccad0be9c53136266bdd9e928
Callum Galbreath <37229146+software-is-art@users.noreply.github.com>
2021-09-02 20:07:18 +1200

    Ng 491 build automation (#3)
    * chore: setup commit message linting
    
    * chore: install semantic-release
    
    * chore: add pre-commit hook to run prettier
    
    * refactor: split AWS SSO logic out of plugin.ts into sso-credentials-cache.ts
    
    This decouples logic making it possible to write tests exercising cache logic
    
    * test: added unit test coverage
    
    Added unit tests using Jest, TS-Jest, and Jest Mock Extended
    
    * ci: added basic GitHub pipeline to begin with
    
    * ci: included semantic-release-dry step for validating PRs
    
    * ci: added release.yml to trigger releases on push to main

---
+++ test/plugin.test.ts
@@ -0,0 +1,38 @@
+import { mock } from "jest-mock-extended";
+import { fetchCredentialsByProfile } from "../dist/plugin";
+import { SsoCredentialsCache } from "../dist/sso-credentials-cache";
+
+describe("Plugin Tests", () => {
+  test("Correct Credential Property Is Returned", async () => {
+    let ssoCredentialsCache = mock<SsoCredentialsCache>();
+    ssoCredentialsCache.get.mockResolvedValue({
+      accessKeyId: "A",
+      secretAccessKey: "B",
+      sessionToken: "C",
+      expiration: undefined,
+    });
+    let result = await fetchCredentialsByProfile(
+      ssoCredentialsCache,
+      "one",
+      "accessKeyId"
+    );
+    expect(result).toBe("A");
+
+    result = await fetchCredentialsByProfile(
+      ssoCredentialsCache,
+      "one",
+      "secretAccessKey"
+    );
+    expect(result).toBe("B");
+
+    result = await fetchCredentialsByProfile(
+      ssoCredentialsCache,
+      "one",
+      "sessionToken"
+    );
+    expect(result).toBe("C");
+
+    expect(ssoCredentialsCache.get).toBeCalledWith("one");
+    expect(ssoCredentialsCache.get).toBeCalledTimes(3);
+  });
+});
---
+++ test/sso-credentials-cache.test.ts
@@ -0,0 +1,119 @@
+import { any, mock, mockFn } from "jest-mock-extended";
+import { CredentialProvider, Credentials } from "@aws-sdk/types";
+import {
+  SsoCredentialsCache,
+  CredentialProviderFactory,
+} from "../src/sso-credentials-cache";
+
+interface MockCredentials {
+  readonly accessKeyId: jest.Mock<string, []>;
+  readonly secretAccessKey: jest.Mock<string, []>;
+  readonly sessionToken: jest.Mock<string | undefined, []>;
+  readonly expiration: jest.Mock<Date | undefined, []>;
+}
+
+function mockProperty<T>(base: object, property: keyof Credentials) {
+  const getter = jest.fn<T, []>();
+  Object.defineProperty(base, property, { get: getter });
+  return getter;
+}
+
+function setupMockCredentials(
+  input: Credentials
+): [Credentials, MockCredentials] {
+  const base = {};
+  const credentials = mock<Credentials>(base);
+  const credentialProperties = {
+    accessKeyId: mockProperty<string>(base, "accessKeyId"),
+    secretAccessKey: mockProperty<string>(base, "secretAccessKey"),
+    sessionToken: mockProperty<string | undefined>(base, "sessionToken"),
+    expiration: mockProperty<Date | undefined>(base, "expiration"),
+  };
+  credentialProperties.accessKeyId.mockReturnValue(input.accessKeyId);
+  credentialProperties.secretAccessKey.mockReturnValue(input.secretAccessKey);
+  credentialProperties.sessionToken.mockReturnValue(input.sessionToken);
+  credentialProperties.expiration.mockReturnValue(input.expiration);
+  return [credentials, credentialProperties];
+}
+
+function setup(
+  input: Credentials = {
+    accessKeyId: "FOO",
+    secretAccessKey: "BAR",
+    sessionToken: "BAZ",
+    expiration: new Date(8640000000000000),
+  }
+): [
+  SsoCredentialsCache,
+  CredentialProviderFactory,
+  CredentialProvider,
+  MockCredentials
+] {
+  const factory = mockFn<CredentialProviderFactory>();
+  const provider = mockFn<CredentialProvider>();
+  const [credentials, mockCredentials] = setupMockCredentials(input);
+  const cache = new SsoCredentialsCache(factory);
+  provider.mockResolvedValue(credentials);
+  factory.calledWith(any()).mockReturnValue(provider);
+  return [cache, factory, provider, mockCredentials];
+}
+
+describe("SSO Credentials Cache Tests", () => {
+  test("Current Session Is Cached Per Profile", async () => {
+    let [cache, , provider] = setup();
+
+    await cache.get("one");
+    await cache.get("one");
+    expect(provider).toBeCalledTimes(1);
+  });
+
+  test("Provider Factory Invoked At Most Once Per Profile", async () => {
+    let [cache, factory] = setup();
+
+    await cache.get("one");
+    await cache.get("one");
+
+    expect(factory).toBeCalledWith({ profile: "one" });
+    expect(factory).toBeCalledTimes(1);
+
+    await cache.get("two");
+
+    expect(factory).toBeCalledWith({ profile: "two" });
+    expect(factory).toBeCalledTimes(2);
+  });
+
+  test("Credential Expiration Is Always Inspected", async () => {
+    let [cache, , , credentials] = setup();
+
+    await cache.get("one");
+    expect(credentials.expiration).toBeCalledTimes(2);
+
+    await cache.get("one");
+    expect(credentials.expiration).toBeCalledTimes(4);
+  });
+
+  test("New Credentials Are Provided Only When Expired", async () => {
+    const expiration = new Date();
+    expiration.setDate(expiration.getDate() + 1);
+
+    const [cache, , provider] = setup({
+      accessKeyId: "A",
+      secretAccessKey: "B",
+      sessionToken: "C",
+      expiration: expiration,
+    });
+
+    await cache.get("one");
+    expect(provider).toBeCalledTimes(1);
+
+    expiration.setDate(expiration.getDate() - 2);
+
+    await cache.get("one");
+    expect(provider).toBeCalledTimes(2);
+
+    expiration.setDate(expiration.getDate() + 2);
+
+    await cache.get("one");
+    expect(provider).toBeCalledTimes(2);
+  });
+});
